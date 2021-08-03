94b9fb8391db942624f5775690b9c5b2c18750ce
Daryl <darylgialolo@gmail.com>
2021-08-03 12:37:55 +0800

    Display All Tasks in a List from Database

--- app/src/main/java/com/myapplicationdev/android/p11_task_manager/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p11_task_manager/MainActivity.java
@@ -1,14 +1,53 @@
 package com.myapplicationdev.android.p11_task_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // List View Components
+    private ListView tasksListView;
+    private ArrayAdapter<Task> arrayAdapter;
+    private ArrayList<Task> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        initListComps();
+        loadTasks();
+    }
+
+    private void initViews() {
+        Button addTaskBtn = findViewById(R.id.add_task_button);
+        addTaskBtn.setOnClickListener(this);
+    }
+
+    private void initListComps() {
+        tasksListView = findViewById(R.id.tasks_list_view);
+        tasks = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, tasks);
+        tasksListView.setAdapter(arrayAdapter);
+    }
+
+    private void loadTasks() {
+        DBHelper db = new DBHelper(this);
+        tasks.addAll(db.getAllTasks());
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO: go to add activity
     }
 }
---
+++ app/src/main/java/com/myapplicationdev/android/p11_task_manager/Task.java
@@ -0,0 +1,28 @@
+package com.myapplicationdev.android.p11_task_manager;
+
+public class Task {
+    private int id;
+    private String name, description;
+    public Task(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s \n%s", id, name, description);
+    }
+}
